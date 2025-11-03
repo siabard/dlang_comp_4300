5baa6540153127cf591a5ac07b974ecf74faa82d
Yeonho Jang <siabard@cardong.co.kr>
2025-11-03 17:34:25 +0900

    movement component

---
+++ source/engine/components/movement_component.d
@@ -0,0 +1,17 @@
+module engine.compoennt.movement_component;
+
+struct MovementComponent {
+  float delta_x;
+  float delta_y;
+}
+
+MovementComponent add_movement(MovementComponent origin, float velocity) {
+  import std.math;
+
+  auto vector_length = sqrt( origin.delta_x * origin_deltal.x + origin.delta_y * origin.delta_y);
+  auto vector = new MovementComponent;
+
+  vector.delta_x = origin.delta_x * velocity / vector_length;
+  vector.delta_y = origin.delta_y * velocity / vector_length;
+  return vector;
+}
